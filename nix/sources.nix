e28543d17175ec6ddab438368112edeb01fc3039
Rory Tyler Hayford <rory.hayford@protonmail.com>
2023-03-07 10:51:36 +0700

    Use a `sha256`

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,1 +1,6 @@
-import (builtins.fetchurl "https://raw.githubusercontent.com/timbertson/niv-util/0bb1e2c9fc3aeeacb97a0aa06b7c380be722b6ea/default.nix")
+import (
+  builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/timbertson/niv-util/0bb1e2c9fc3aeeacb97a0aa06b7c380be722b6ea/default.nix";
+    sha256 = "sha256-BPZeXf6nN29YL2/JuVzqaIcyPLnYzQFrZgwTy5lMzPY=";
+  }
+)
